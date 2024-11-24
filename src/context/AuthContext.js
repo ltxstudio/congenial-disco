c87d8f3526e237b44266d40001a98837d88b3154
Ltxstudio <ltxstudio.info@gmail.com>
2024-11-24 21:22:51 +0600

    Create AuthContext.js

---
+++ src/context/AuthContext.js
@@ -0,0 +1,26 @@
+import React, { createContext, useState, useEffect } from 'react';
+import { auth, onAuthStateChanged } from '../firebaseConfig';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
